6ab438410c703cd5a09a1b71bb60a000346a637b
ulkarchbnv <ulkar.roman.23@gmail.com>
2026-03-06 19:06:23 +0100

    Displaying recipe response from FastAPI in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   const [ingredients, setIngredients] = useState("");
   const [preferences, setPreferences] = useState("");
   const [allergies, setAllergies] = useState("");
+  const [recipe, setRecipe] = useState(null);
   const generateRecipe = async () => 
   {
     console.log("Button clicked");
@@ -14,10 +15,22 @@ function App() {
       
     };
     console.log("Payload being sent: ", load)
+    const response = await fetch("http://127.0.0.1:8000/generate-recipe", 
+      {
+        method: "POST",
+        headers: 
+        {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(load)
+      }
+    );
+    const data = await response.json();
+    console.log("Response from backend:", data);
+    setRecipe(data);
   };
   return (
-    <div>
-      <h1>AI Cookbook</h1>
+    <div>      <h1>AI Cookbook</h1>
       <label>Ingredients</label>
       <br />
       <input
@@ -52,6 +65,28 @@ function App() {
       <p>Ingredients: {ingredients}</p>
       <p>Preferences: {preferences}</p>
       <p>Allergies: {allergies}</p>
+            {recipe && (
+        <div>
+          <h2>{recipe.title}</h2>
+
+          <p><strong>Ingredients:</strong> {recipe.ingredients.join(", ")}</p>
+          <p><strong>Preferences:</strong> {recipe.preferences.join(", ")}</p>
+          <p><strong>Allergies:</strong> {recipe.allergies.join(", ")}</p>
+
+          <h3>Steps</h3>
+          <ul>
+            {recipe.steps.map((step, index) => (
+              <li key={index}>{step}</li>
+            ))}
+          </ul>
+
+          <h3>Nutrition Estimate</h3>
+          <p>Calories: {recipe.nutrition_estimate.calories}</p>
+          <p>Protein: {recipe.nutrition_estimate.protein}</p>
+          <p>Carbs: {recipe.nutrition_estimate.carbs}</p>
+          <p>Fat: {recipe.nutrition_estimate.fat}</p>
+        </div>
+      )}
     </div>
   );
 }
